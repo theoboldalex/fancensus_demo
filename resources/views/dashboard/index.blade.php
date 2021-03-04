356b25f9d3b0625e7307ed4ae570cbaf65d175c5
theoboldalex <theoboldalex@gmail.com>
2021-03-04 13:58:35 +0000

    handle errors on create form

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -21,11 +21,19 @@
                 <div class="w-full md:flex justify-between">
                     <div class="flex flex-col md:w-6/12 md:mr-4 my-4">
                         <label for="link_name">Link Name</label>
-                        <input type="text" name="link_name" placeholder="TwitterBook" class="p-2 rounded-lg">
+                        <input type="text" name="link_name" placeholder="TwitterBook" class="p-2 rounded-lg @error('link_name') border border-red-500 @enderror">
+                        @error('link_name')
+                            <small class="text-red-500">
+                                {{ $message }}
+                            </small>
+                        @enderror
                     </div>
                     <div class="flex flex-col md:w-6/12 md:ml-4 my-4">
                         <label for="link_url">Link URL</label>
-                        <input type="text" name="link_url" placeholder="https://twitterbook.com/username" class="p-2 rounded-lg">
+                        <input type="text" name="link_url" placeholder="https://twitterbook.com/username" class="p-2 rounded-lg @error('link_url') border border-red-500 @enderror">
+                        @error('link_url')
+                            <small class="text-red-500">{{ $message }}</small>
+                        @enderror
                     </div>
                 </div>
                 <button type="submit" class="w-full my-4 rounded-lg text-white font-light py-2 px-4 bg-blue-400 hover:bg-blue-500 transition duration-300 ease">Save Link</button>
